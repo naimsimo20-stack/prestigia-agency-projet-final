24c9236c91d879b664639e7827034b4a162dc3e0
naimsimo20-stack <naimsimo20@gmail.com>
2025-12-06 17:49:41 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,7 +70,7 @@ export const metadata: Metadata = {
     other: [
       {
         rel: "icon",
-        url: "/prestigia-logo.png",
+        url: "/prestigia-logo-32.png",
         sizes: "32x32",
         type: "image/png",
       },
