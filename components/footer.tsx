b7deedfea7878002f2f1557215b779f745280b28
naimsimo20-stack <naimsimo20@gmail.com>
2025-12-12 16:17:07 +0100

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -32,10 +32,10 @@ export default function Footer() {
   ]
 
   const socialLinks = [
-    { label: "Facebook", href: "https://facebook.com/prestigia" },
-    { label: "Instagram", href: "https://instagram.com/prestigia" },
-    { label: "LinkedIn", href: "https://linkedin.com/company/prestigia" },
-    { label: "Twitter", href: "https://twitter.com/prestigia" },
+    { label: "Facebook", href: "https://web.facebook.com/profile.php?id=61584253697576&locale=fr_FR" },
+    { label: "Instagram", href: "https://instagram.com/prestigia__agency" },
+    { label: "LinkedIn", href: "https://linkedin.com/company/prestigia agency" },
+    { label: "Twitter", href: "https://twitter.com/prestigia agency" },
   ]
 
   const handleNavClick = (href: string) => {
